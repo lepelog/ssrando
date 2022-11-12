568ae638bbe352c7a9794cd801a45b5dffb235b9
lepelog <25211966+lepelog@users.noreply.github.com>
2022-11-12 13:07:40 +0100

    tmp

--- graph_logic/pools.py
+++ graph_logic/pools.py
@@ -12,7 +12,7 @@ DUNGEON_REVERSE_ENTRANCES_POOL = {
 
 VANILLA_DUNGEON_ACCESSES = {
     SV: EIN("Faron - Deep Woods - Exit to Skyview Temple"),
-    ET: EIN("Eldin - Volcano - Exit to Earth Temple"),
+    ET: EIN("Eldin - Eldin Volcano - Exit to Earth Temple"),
     LMF: EIN("Lanayru - Desert - Exit to Lanayru Mining Facility"),
     AC: EIN("Faron - Lake Floria - Exit to Ancient Cistern"),
     SSH: EIN("Lanayru - Sand Sea - Sandship Dock Exit"),
@@ -52,9 +52,9 @@ SILENT_REALM_EXITS_POOL = {
 
 SILENT_REALM_ACCESSES = {
     EIN("Skyloft - Trial Gate Exit"),
-    EIN("Faron - Woods - Trial Gate Exit"),
+    EIN("Faron - Faron Woods - Trial Gate Exit"),
     EIN("Lanayru - Desert - Trial Gate Exit"),
-    EIN("Eldin - Volcano - Trial Gate Exit"),
+    EIN("Eldin - Eldin Volcano - Trial Gate Exit"),
 }
 SILENT_REALM_ACCESSES_POOL = {
     exit: PoolExit(exit, [entrance_of_exit(exit)]) for exit in SILENT_REALM_ACCESSES
